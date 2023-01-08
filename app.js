729b80218905fe14f4616ab4477332e6956e7fc4
Konstantin <kvoronin161gmail.com>
2023-01-08 08:17:38 +0300

    Add env to project

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const TelegramBot = require("node-telegram-bot-api");
+const TelegramBot = require('node-telegram-bot-api')
+const { parsed } = require('dotenv').config()
 
-const token = "YOUR_TELEGRAM_BOT_TOKEN";
+const token = parsed.TOKEN || ''
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true })
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
+bot.on('message', msg => {
+  const chatId = msg.chat.id
 
-  bot.sendMessage(chatId, "Received your message");
-});
+  bot.sendMessage(chatId, 'Received your message')
+})
+
+/**
+ * Application start
+ */
+const appLaunch = () => {
+  try {
+    console.log('Telegram bot has been started')
+  } catch (error) {
+    console.log('An error occurred while trying to launch the app. Error: ' + error)
+  }
+}
+
+appLaunch()
